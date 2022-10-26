27605dc2eb70c8465d6b9d145e3dffda33244e77
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-10-26 15:40:15 +0300

    add event handlers for enlarged and default font size

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,18 @@ let darkMode = window.document.querySelector("button#darkMode");
 darkMode.onclick = function() {  
   body.style.backgroundColor = "black";
   body.style.color = "white";
-}
+};
 let lightMode = window.document.querySelector("button#lightMode");
 lightMode.onclick = function()  {
   body.style.backgroundColor = "white";
   body.style.color = "black";
-}
+};
+let increasedFontSize = window.document.querySelector("button#increasedFontSize");
+increasedFontSize.onclick = function()  {
+  body.style.fontSize = "1.8em";
+};
+let defaultFontSize = window.document.querySelector("button#defaultFontSize");
+defaultFontSize.onclick = function () {
+body.style.fontSize = null;
+};
 };
