38ed287b96ce08d22f7d7ac424cfba0722fdf5ec
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2022-10-26 15:19:51 +0300

    add event handlers for darkMode and lightMode IDs

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 //User interface logic
 window.onload = function()  {
-
+let body = window.document.querySelector("body");
+let darkMode = window.document.querySelector("button#darkMode");
+darkMode.onclick = function() {  
+  body.style.backgroundColor = "black";
+  body.style.color = "white";
+}
+let lightMode = window.document.querySelector("button#lightMode");
+lightMode.onclick = function()  {
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+}
 };
